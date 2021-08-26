0694ef76572ab9c1852d5d813d81721cdaed446f
MyoMaung <myomaung5255225@gmail.com>
2021-08-27 00:50:33 +0630

    add_ui_dashboard

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 import express from 'express'
 import cors from 'cors'
 import path from 'path'
+import favicon from 'express-favicon'
 //importing from db
 import { connectDB } from './db'
 
@@ -11,7 +12,9 @@ import { server_port } from './config'
 const app = express()
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.use('', express.static(path.join(__dirname, '..', 'public')))
+app.use(favicon(path.join(__dirname, '..', 'public/favicon.ico')))
 app.use(express.json())
 
 const startApp = () => {
