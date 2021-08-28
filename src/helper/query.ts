77c100821ba9e8fae1ad5eb6189510b1f8f847cb
MyoMaung <myo.maung@xenera.jp>
2021-08-28 18:38:38 +0630

    update_query

--- src/helper/query.ts
+++ src/helper/query.ts
@@ -1,14 +1,28 @@
 import { Currency, Rate } from "../models"
 
 export const getHookRates = async (search_text: string) => {
-    const currency: any = await Currency.findOne({ name: { $regex: '.*' + search_text + '.*' } });
-    if (currency) {
-        const rates = await Rate.find({ from_currency: currency._id }).populate('currency').populate('user')
+    let word_count = search_text.split(' ');
+    if (word_count.length > 1) {
+        const base_currency: any = await Currency.findOne({ keyword: { $regex: '.*' + word_count[0]?.toLocaleLowerCase() + '.*' } });
+        const to_currency: any = await Currency.findOne({ keyword: { $regex: '.*' + word_count[2]?.toLocaleLowerCase() + '.*' } });
+        let query: any = {};
+        if (base_currency) {
+            query["from_currency"] = base_currency;
+        }
+        if (to_currency) {
+            query["to_currency"] = to_currency;
+        }
+        const rates: any = await Rate.find(query).populate('currency').populate('user');
         if (rates) {
             return rates;
+        }
+        else {
+            return
         }
     }
     else {
         return;
     }
+
+
 }
