a7ef9898144ac95c2548ae83b75162463ba18d75
MyoMaung <myo.maung@xenera.jp>
2021-08-30 15:35:06 +0630

    ok

--- src/helper/query.ts
+++ src/helper/query.ts
@@ -7,10 +7,10 @@ export const getHookRates = async (search_text: string) => {
         const to_currency: any = await Currency.findOne({ keyword: { $regex: '.*' + word_count[2]?.toLocaleLowerCase() + '.*' } });
         let query: any = {};
         if (base_currency) {
-            query["from_currency"] = base_currency;
+            query["from_currency"] = base_currency._id;
         }
         if (to_currency) {
-            query["to_currency"] = to_currency;
+            query["to_currency"] = to_currency._id;
         }
         const rates: any = await Rate.find(query).populate('currency').populate('user');
         if (rates) {
